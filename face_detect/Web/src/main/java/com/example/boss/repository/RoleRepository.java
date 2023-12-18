3387ec1ac98c785ec670ca25bb5f43d21acf19e6
Thanh Dinh <toygungun4@gmail.comgit config --global user.name Thanh>
2023-12-18 21:02:29 +0700

    update

--- face_detect/Web/src/main/java/com/example/boss/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.boss.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.boss.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-	Role findByName(String name);
-}
